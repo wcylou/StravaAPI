4518a89c0aefe4351a22020bdc161aa6771559fb
Wilson <wilson_lou@hotmail.co.uk>
2018-08-26 15:39:23 +0100

    opened httpClient

--- StravaApp/src/com/wilsonlou/strava/StravaTracker.java
+++ StravaApp/src/com/wilsonlou/strava/StravaTracker.java
@@ -1,6 +1,59 @@
 package com.wilsonlou.strava;
 
-public class StravaTracker {
-	
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class StravaTracker {
+	public boolean authorise = true;
+
+	public static void main(String[] args) {
+
+		CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+		
+		try {
+			HttpGet httpGetRequest = new HttpGet("https://www.strava.com/api/v3/athletes/22467435?access_token=5746029cb8169581d4528a663f3229dccec5e3fd");
+			HttpResponse httpResponse = httpClient.execute(httpGetRequest);
+
+			System.out.println("----------------------------------------");
+			System.out.println(httpResponse.getStatusLine());
+			System.out.println("----------------------------------------");
+
+			HttpEntity entity = httpResponse.getEntity();
+
+			byte[] buffer = new byte[1024];
+			if (entity != null) {
+				InputStream inputStream = entity.getContent();
+				try {
+					int bytesRead = 0;
+					BufferedInputStream bis = new BufferedInputStream(inputStream);
+					while ((bytesRead = bis.read(buffer)) != -1) {
+						String chunk = new String(buffer, 0, bytesRead);
+						System.out.println(chunk);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						inputStream.close();
+					} catch (Exception ignore) {
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				httpClient.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
